9d7008499cc2ad4bdb3a1ba201211b13e0da8f52
Vovkulaka <dmyterko-jw@ukr.net>
2019-02-08 12:12:49 +0200

    Add project files.

---
+++ GroupingDataInTable/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GroupingDataInTable.Startup))]
+namespace GroupingDataInTable
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
